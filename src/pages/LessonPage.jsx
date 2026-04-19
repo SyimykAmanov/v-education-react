2bdf74daf70205a4279875b4a1c2b5ee1a33deb8
SyimykAmanov <ulanbayuuly@gmail.com>
2026-04-20 00:22:46 +0200

    feat: add LessonPage with video player and lesson details
    - extract subjectId and lessonId from URL using useParams
    - fetch lessons from API using useEffect
    - display lesson title, video iframe and description
    - add loading and not-found states

--- src/pages/LessonPage.jsx
+++ src/pages/LessonPage.jsx
@@ -1,3 +1,54 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function LessonPage() {
-    return <h1>Lesson Page</h1>
+    const {subjectId, lessonId} = useParams();
+    const [lessons, setLessons] = useState([]);
+    const [isLoading, setIsloading] = useState(true);
+    const lesson = lessons.find(lesson => lesson.id == lessonId && lesson.subjectId == subjectId);
+
+    useEffect(() => {
+        async function fetchLessons() {
+            const lessonsURL = "http://127.0.0.1:3658/m1/1236529-1233135-default/lessons";
+            const response = await fetch(lessonsURL);
+            const result = await response.json();
+            setLessons(result.lessons);
+        };
+
+        async function fetchData() {
+            await fetchLessons();
+            setIsloading(false);
+        }
+        
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <p>Wird geladen...</p>
+    }
+
+    if (!lesson) {
+        return <p>Урок не найден</p>;
+    }
+    
+    return (
+        <article className="lesson">
+            <div className="lesson__header">
+            <h1 className="lesson__title">{lesson.order}. {lesson.title}</h1>
+            </div>
+
+            <div className="lesson__video">
+                <iframe className="lesson__iframe"
+                    src={lesson.videoUrl}
+                    frameBorder="0"
+                    allowFullScreen>
+                </iframe>
+            </div>
+
+            <div className="lesson__description">
+                <h2 className="lesson__subtitle">Lektionbeschreibung</h2>
+                <p className="lesson__text">{lesson.description}</p>
+            </div>
+        </article>
+    )
 }
